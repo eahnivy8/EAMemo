411901fb1506b5a26f0948fa7538fde58fe2283e
Eddie <eahnivy8@gmail.com>
2020-01-10 13:39:46 +0900

    Initial Commit

---
+++ EAMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EAMemo
+//
+//  Created by Eddie Ahn on 2020/01/10.
+//  Copyright © 2020 Sang Wook Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
